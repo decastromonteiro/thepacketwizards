94aee0a6d27a8688844e233e519e19d55cb45a0d
Leonardo Monteiro <decastromonteiro@gmail.com>
2020-04-05 11:35:58 -0300

    migrations added to git repo

--- web/thepacketwizards/__init__.py
+++ web/thepacketwizards/__init__.py
@@ -1,6 +1,4 @@
-# todo: add tags to blog posts
-# todo: fix twitter card image not showing
+# todo: create share buttons for each blogspot
 # todo: create related posts
 # todo: create search feature
-# todo: create share buttons for each blogspot
 # todo: convert index of blogposts into bootstrap cards
